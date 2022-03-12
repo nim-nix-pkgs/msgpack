50048b5dce08b1c82020a874a98d8859985aed4e
hugosenari <hugosenari@gmail.com>
2022-03-12 03:37:50 -0300

    chore: re index msgpack

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A MessagePack binding for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."msgpack-master".url = "path:./master";
+  inputs."msgpack-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."msgpack-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
